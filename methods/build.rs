d9beb4f018009b295fc36f2698c2c2c035450396
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-08-30 20:28:09 -0300

    Small fix

--- methods/build.rs
+++ methods/build.rs
@@ -1,7 +1,3 @@
-use std::collections::HashMap;
-
 fn main() {
-    let mut methods = HashMap::new();
-    methods.insert("guest", "guest");
-    risc0_build::embed_methods_with_options(methods);
+    risc0_build::embed_methods();
 }
